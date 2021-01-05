7ce6fdef31436b6bc2c9e4fc9b3e644ea8ba5a29
piotr_z6@wp.pl <Wd7BWfcmH9pqauv>
2021-01-05 15:06:19 +0100

    first commit

---
+++ src/main/java/com/example/Jenkinsdemoapp/JenkinsDemoAppApplication.java
@@ -0,0 +1,20 @@
+package com.example.Jenkinsdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoAppApplication.class, args);
+	}
+
+	@GetMapping
+	public String get() {
+		return "No Witam!";
+	}
+}
